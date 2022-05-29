e06fa648124c3c37e41a8ac658344dad56069ab2
Raman <96678794+Raman84@users.noreply.github.com>
2022-05-29 15:09:11 +0530

    perms fixed

--- event/src/reducers/network.js
+++ event/src/reducers/network.js
@@ -9,7 +9,7 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case CHANGE_NETWORK:
             console.log("IN CHANGE_NETWORK");
-            console.log("CHANGE_NETWORK PAYLOAD", action);
+//             console.log("CHANGE_NETWORK PAYLOAD", action);
             console.log("CHANGE_NETWORK STATE", state);
             return {
                 ...state,
